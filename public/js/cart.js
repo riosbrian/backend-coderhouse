df200c8128ff1960844195d454993b9908600631
Brian Rios <riosbrian.ar@gmail.com>
2023-11-27 13:58:01 +0100

    funcionalidad de carrito js

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,7 +1,34 @@
 const btns = document.querySelector('.item-card__actions');
+const ROUTE = `http://localhost:8081/api/carts/`;
 
-btns.addEventListener('click', (e) => {
+btns.addEventListener('click', async (e) => {
   if (e.target.tagName !== 'BUTTON') return;
   const action = e.target.getAttribute('do');
-  console.log(action);
+  const id = e.target.getAttribute('id');
+  let response;
+  try {
+    if (action === 'sub') {
+      const res = await fetch(`${ROUTE}${id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-type': 'application/json',
+        },
+      });
+      const { data } = await res.json();
+      console.log(data);
+      response = data.products.map((item) => ({
+        ...item.product,
+        quantity: item.quantity,
+      }));
+    }
+  } catch (error) {
+    console.log(error);
+  }
+  /*  {
+    try {
+      const res = await ;
+    } catch (error) {
+      console.log(error);
+    }
+  } */
 });
